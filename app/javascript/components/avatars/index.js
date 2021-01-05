d025aeb625f19144972f1a532dcc03300ffe0932
Gabriel Fontes <gabrielferroroque@hotmail.com>
2021-01-04 21:28:19 -0300

    BTNDOM function implemented

--- app/javascript/components/avatars/index.js
+++ app/javascript/components/avatars/index.js
@@ -4,21 +4,25 @@ import initializeAssetsForColor from './initialize/skincolor/initialize_assets_f
 import initializeColor from './initialize/color/initialize_color';
 
 import { getGender, getAssetsInfo } from './initialize/get_assets_info';
+import takeBtnFromDom from './initialize/take_btn_from_dom';
 
 const avatarCreator = () => {
+  let btn;
   window.onload = function() {
-    // Getting img elements and constructing avatar canvas for the 1st time
+    // Getting img elements and constructing avatar canvas and assets for 1st time
     const avDom = grabElements();
     updateCanvas(avDom);
-
+    // Getting avatar gender and all available assets for matching gender
     const avGender = getGender();
     const assets = getAssetsInfo(avGender.allInfo, avGender.info)
-
+    // Getting all the current available colors for current assets
     const currentFile = avDom.imgBase.src.slice(avDom.imgBase.src.lastIndexOf("/") + 1);
     const filteredAssets = initializeAssetsForColor(currentFile, assets);
     const assetColor = initializeColor(avDom, assets);
-    console.log(assetColor);
+    btn = takeBtnFromDom();
   }
+
+console.log(btn);
 }
 
 export default avatarCreator;
