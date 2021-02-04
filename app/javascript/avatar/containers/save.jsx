2cdadd5ac8640009f95b5eb25cf88452efad9d8a
Gabriel Fontes <gabrielferroroque@hotmail.com>
2021-02-03 23:44:15 -0300

    Form added

--- app/javascript/avatar/containers/save.jsx
+++ app/javascript/avatar/containers/save.jsx
@@ -5,8 +5,19 @@ import { changeCategory, showItems, selectItem } from '../actions/index';
 
 
 class Save extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      assets: null,
+      colors: null,
+      img: null
+    }
+  }
+
   handleClick = () => {
     // TODO: Implement saving
+
 
   }
 
@@ -14,8 +25,11 @@ class Save extends Component {
     return (
       <div className="studio-save-btn" onClick={this.handleClick} >
         <img src="./avatar/buttons/save.png" alt="Save Changes Button" />
-        <form action={`./avatars/${id}`} method="POST">
-
+        <form action={`./avatars/${id}`} method="POST" ref={r => this.form = r}>
+          <input type="hidden" name="authenticity_token" value={this.props.token} />
+          <input type="hidden" name="avatar[img]" id="avatar_img" value={this.state.img} />
+          <input type="hidden" name="avatar[assets]" id="avatar_assets" value={this.state.assets} />
+          <input type="hidden" name="avatar[colors]" id="avatar_colors" value={this.state.colors} />
         </form>
       </div>
     );
@@ -23,7 +37,7 @@ class Save extends Component {
 }
 
 function mapStateToProps(state) {
-  return { selectedCategory: state.selectedCategory, integrants: state.integrants, layers: state.avatarLayers };
+  return { layers: state.avatarLayers };
 }
 
 function mapDispatchToProps(dispatch) {
