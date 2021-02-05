3b256fcbedbde649a3c49eb9ef158ef3c07b0934
Gabriel Fontes <gabrielferroroque@hotmail.com>
2021-02-05 00:10:02 -0300

    Useless files deleted

--- app/javascript/components/avatars/change_gender.js
+++
@@ -1,5 +0,0 @@
-export default function changeGender(gender) {
-  let form = document.querySelector(".changeGender");
-  document.getElementById("avatar_gender").value = gender;
-  form.submit();
-}
