8aab84a245314bf5e8a868a6980a8086b900273d
Gabriel Fontes <gabrielferroroque@hotmail.com>
2021-02-09 18:02:51 -0300

    ChatRoom Controller edited

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -2,10 +2,8 @@ class ChatRoomsController < ApplicationController
   before_action :set_present_chats, only: [:index]
   before_action :fix_params, only: [:create]
   before_action :set_user
-  before_action :set_present_chats, only: [:create, :show, :index]
 
   def index
-    # Where it is going to display all chatrooms for current user
     @chat_room = ChatRoom.new
     authorize @chat_room
   end
@@ -16,12 +14,11 @@ class ChatRoomsController < ApplicationController
     @participants = @chat_room.users
     @messages = @chat_room.messages
     @new_message = Message.new
-    @user = current_user
   end
 
   def create
     puts params[:chat_room]
-    @chat_room_form = ChatRoomForm.new(user_ids: params[:chat_room][:user_ids].push(@user.id))
+    @chat_room_form = ChatRoomForm.new(user_ids: chat_room_params[:user_ids].push(@user.id))
     if @chat_room_form.valid?
       @chat_room = @chat_room_form.submit
       redirect_to @chat_room
@@ -38,10 +35,7 @@ class ChatRoomsController < ApplicationController
   private
 
   def set_present_chats
-    @present_chats = []
-    Participant.where(user_id: current_user).each do |participant|
-      @present_chats << participant.chat_room
-    end
+    @present_chats = Participant.find_user_participation(@user)
   end
 
   def fix_params
@@ -54,9 +48,5 @@ class ChatRoomsController < ApplicationController
 
   def chat_room_params
     params.require(:chat_room).permit(user_ids: [])
-  end
-
-  def number?(string)
-    true if Float(string) rescue false
   end
 end
